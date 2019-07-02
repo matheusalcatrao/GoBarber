6913203c33bf069af20e54134ef7e8f1534ea961
Matheus Alcatrão <Matheus Alcatrao>
2019-07-02 16:56:15 -0300

    Auth:  jwt and Middleware

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -6,10 +6,40 @@ class UserController {
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
-      return res.status(400).json({ error: 'This user already exists.' });
+      return res.status(401).json({ error: 'This user already exists.' });
     }
 
     const { id, name, email, provider } = await User.create(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async update(req, res) {
+    console.log(req.id);
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.id);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(401).json({ error: 'This user already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
     return res.json({
       id,
       name,
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '3819dea8f1f71e34b8716ae533c81b6c',
+  expiresIn: '7d',
+};
