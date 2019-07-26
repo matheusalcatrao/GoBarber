e58709896a5af903ea23bca173c7dea5429fa196
Matheus Alcatrão <Matheus Alcatrao>
2019-07-26 17:28:07 -0300

    Filas

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,7 +6,8 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import NotificationSchema from '../schemas/Notification';
-import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import CancelationEmail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async index(req, res) {
@@ -113,6 +114,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -132,10 +138,8 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name}<${appointment.provider.email}>`,
-      subject: 'Agendamento Cancelado',
-      text: 'Você tem uma novo cancelamento',
+    await Queue.add(CancelationEmail.key, {
+      appointment,
     });
 
     return res.json(appointment);
